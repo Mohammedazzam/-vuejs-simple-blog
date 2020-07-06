8c61d528a73e0cec35726471c62f1b76ef713675
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-07-06 11:34:38 +0300

    (3 Header part1)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Vue.js App"/>
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/app-header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
 }
 </style>
---
+++ src/components/app-header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>Blog</h1>
+    </header>
+</template>
+
+<style >
+    header{
+        height: 250px;
+        background-color:#eee ;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-image: url(../assets/header-bg.jpg);
+     background: #330033; /* Old browsers */ 
+      background: -moz-linear-gradient(-45deg, #330033 0%, #333366 100%); /* FF3.6-15 */
+      background: -webkit-linear-gradient(-45deg, #330033 0%,#333366 100%); /* Chrome10-25,Safari5.1-6 */
+      background: linear-gradient(135deg, #330033 0%,#333366 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+      filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#330033', endColorstr='#333366',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+    }
+    h1{
+        margin: 0;
+    }
+</style>
+
+<script>
+export default {
+    
+}
+</script>
