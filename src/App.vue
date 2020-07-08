1828b92fc65f509f9298afc847b7a884d29026ae
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-07-08 13:13:05 +0300

    (تغيير الصفحات)

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,16 @@
        v-for="blog in blogs" :key="blog.id"
        :data="blog"></blog-card>
 
-
+    
     </div>
+
+    <div class="text-center">
+      <button type="button" class="load-more-button" @click="loadeMore">
+      Load More
+      </button>
+    </div>
+
+
   </div>
 </template>
 
@@ -22,19 +30,31 @@ export default {
   name: 'App',
   data(){
     return{
-      blogs:null
+      blogs:null,
+      page:1
     }
   },
   components: {
     AppHeader,
     BlogCard
   },
+
   mounted(){
-    axios.get('http://localhost:3000/blog?_page=1&_limit=6').then((res)=>{//('http://localhost:3000/blog')    ('http://localhost:3000/blog?_page=2&_limit=6')
+    this.getBlogs(this.page);
+  },
+
+  methods:{
+    getBlogs(page){
+          axios.get(`http://localhost:3000/blog?_page=${page}&_limit=6`).then((res)=>{//('http://localhost:3000/blog')    ('http://localhost:3000/blog?_page=2&_limit=6')
 
       // console.log(res.data)
       this.blogs = res.data
     })
+    },
+    loadeMore(){
+      this.page++;
+      this.getBlogs(this.page)
+    }
   }
 }
 </script>
@@ -53,5 +73,19 @@ body{
   display: flex;
   flex-wrap:wrap ;
 
+}
+.text-center{
+  text-align: center;
+}
+.load-more-button{
+  color: #fff;
+  background-color: #990099;
+  border-radius: 5px;
+  border: none;
+  padding: 10px 25px;
+  font-size: 18px;
+  line-height: 1;
+  margin-top: 50px;
+  margin-bottom: 50px;
 }
 </style>
