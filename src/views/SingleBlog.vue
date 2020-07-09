2b658bdd3210b8fa4ec45e1c028f4782474ccc35
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-07-09 11:11:01 +0300

    (11  Single Blog Page)

---
+++ src/views/SingleBlog.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-if="blog">
+        <p>{{blog.title}}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            blog:null
+        }
+    },
+    mounted(){
+        axios.get('http://localhost:3000/blog/'+this.$route.params.id).then((res) => {
+            // console.log(res.data)
+            this.blog = res.data;
+            
+        });
+
+    }
+}
+</script>
+
+
+
+<style lang="">
+    
+</style>
